f080c0264b40cbab67447ad35f24ffb0a39f513b
ABMA16 <abraham-m-a@outlook.com>
2024-04-24 18:23:03 -0600

    Addition of the log feature. Now the program generates a .txt logfile at the end of the execution

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
-import com.google.gson.JsonArray;
-import com.google.gson.JsonParser;
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
@@ -12,8 +15,8 @@ public class Main {
         URL myURL = new URL("https://es.iban.com/currency-codes");
 
         while (status == 1) {
-            System.out.println("\n***************************************************************************************");
-            System.out.println("These are the acronyms for some of the most used currencies in latinamerica:  \n" +
+            PrintAndLog("\n***************************************************************************************\n" +
+                    "These are the acronyms for some of the most used currencies in latinamerica:  \n" +
                     "ARS : Argentinian peso \n" +
                     "BOB : Bolivian bolivar \n" +
                     "BRL : Brazilian Real \n" +
@@ -23,40 +26,73 @@ public class Main {
                     "EUR : Euro \n" +
                     "MXN : Mexican peso \n" +
                     "If the currency you are looking for is not listed, please remit to the following URL with " +
-                            "a more complete list: " + myURL);
-            System.out.println("***************************************************************************************");
+                            "a more complete list: " + myURL + "\n" +
+                    "***************************************************************************************");
+
             Scanner scan = new Scanner(System.in);
-            System.out.println("Please write the 3 letter acronym for your coins: ");
+            PrintAndLog("Please write the 3 letter acronym for your coins: ");
             var inputCurrency = scan.nextLine().toUpperCase();
-            System.out.println("Please write the 3 letter acronym of the coin you want to consult: ");
+            WriteToLog(inputCurrency);
+
+            PrintAndLog("Please write the 3 letter acronym of the coin you want to consult: ");
             var outputCurrency = scan.nextLine().toUpperCase();
+            WriteToLog(outputCurrency);
 
             try {
                 double rate = Requester.searchCurrency(inputCurrency, outputCurrency);
                 if (rate > 0) {
-                    System.out.println("The exchange rate is: " + rate);
-                    System.out.println("Please introduce the amount of " + inputCurrency + " you want to exchange for " + outputCurrency + ":");
+                    PrintAndLog("The exchange rate is: " + rate);
+                    PrintAndLog("Please introduce the amount of " + inputCurrency + " you want to exchange for " + outputCurrency + ":");
                     double amount = scan.nextDouble();
-                    System.out.println(amount + " " + inputCurrency + " is equal to " + amount*rate + " " + outputCurrency);
+                    WriteToLog(amount + " " + inputCurrency);
+                    PrintAndLog(amount + " " + inputCurrency + " is equal to " + amount*rate + " " + outputCurrency);
                 } else if (rate == 0){
-                    System.out.println("I could not find the second currency, please try again.");
+                    PrintAndLog("I could not find the second currency, please try again.");
                 } else {
-                    System.out.println("I could not find the original currency");
+                    PrintAndLog("I could not find the original currency");
                 }
 
-                System.out.println("Would you like to do a new conversion? 1 = yes, 2 = no");
+                PrintAndLog("Would you like to do a new conversion? 1 = yes, 2 = no");
                 status = scan.nextInt();
+                WriteToLog(Double.toString(status));
 
             } catch (NumberFormatException e) {
-                System.out.println("Currency not found " + e.getMessage());
+                PrintAndLog("Currency not found " + e.getMessage());
             } catch (RuntimeException e) {
-                System.out.println(e.getMessage());
-                System.out.println("Application finished.");
+                PrintAndLog(e.getMessage());
+                PrintAndLog("Application finished.");
             }
         }
 
-        System.out.println("***************************************************************************************");
-        System.out.println("-----------------------THANKS FOR USING OUR CONVERSION SERVICES-------------------------");
-        System.out.println("***************************************************************************************");
+        PrintAndLog("***************************************************************************************");
+        PrintAndLog("-----------------------THANKS FOR USING OUR CONVERSION SERVICES-------------------------");
+        PrintAndLog("***************************************************************************************");
+    }
+
+    private static void PrintAndLog(String message) {
+        System.out.println(message);
+        WriteToLog(message);
+    }
+
+    private static void WriteToLog(String message) {
+        LocalDate today = LocalDate.now();
+        String formatedDate = today.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        String fileName = "logger_" + formatedDate + ".txt";
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
+            LocalDateTime currentDateTime = LocalDateTime.now();
+            String format = currentDateTime.format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+
+            writer.append(format + " : ");
+            writer.append(message + "\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Exception writing logs " + e.getMessage());
+        }
     }
 }
+
+
+
